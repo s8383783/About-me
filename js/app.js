6630522a1870c354ccbdb9121b9059120c90b5d0
Shania Kimbrough <shania_nicolebb@yahoo.com>
2021-07-14 21:04:42 -0700

    Draft

---
+++ js/app.js
@@ -0,0 +1,26 @@
+let yourName = prompt('What is your name ?');
+document.write('Hi there,' + ' ' + yourName);
+alert('Now for a quick quiz!');
+
+
+// Question 1
+let Army = prompt('Was Shania in the army?');
+let lowArmy = Army.toLowerCase();
+if (lowArmy === 'yes' || lowArmy === 'y') {
+  alert('You are correct! I served for 6 years active duty. Then contracted for the government overseas!');
+}
+// Question 2
+let color = prompt('Is my favorite color green?');
+let lowColor = color.toLowerCase();
+if (lowColor === 'yes' || color === 'y') {
+  alert('You are correct! I hate purple btw');
+}
+// Question 3
+let bestState = prompt('Is Texas the greatest state in the entire world?');
+let lowbestState = bestState.toLowerCase;
+if (lowbestState === 'yes' || lowbestState === 'y') {
+  alert('You are correct!');}
+
+//Question 4
+
+// Question 5
